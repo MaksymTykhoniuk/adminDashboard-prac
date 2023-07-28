e75796f5a8267fded00d24d09b2ea70d8eb98ea1
MaksymTykhoniuk <mxtkhnk@gmail.com>
2023-07-28 18:32:29 +0300

    add dist

---
+++ dist/assets/Products-d49f8362.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-4e38b42b.js";const s=()=>r.jsx("div",{children:"Products"});export{s as default};
---
+++ dist/assets/Users-20e235d6.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-4e38b42b.js";const e=()=>s.jsx("div",{children:"Users"});export{e as default};
