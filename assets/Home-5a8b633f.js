35bfba988477f002289774a82272b5cde4028a2d
MaksymTykhoniuk <MaksymTykhoniuk@users.noreply.github.com>
2023-07-29 17:04:51 +0000

    deploy: c350969366bc9158e71ea48440c06db836551bf5

---
+++ assets/Home-5a8b633f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-6d19d55c.js";const r=()=>e.jsx("div",{children:"Home"});export{r as default};
---
+++ assets/Products-b3e0be15.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-6d19d55c.js";const s=()=>r.jsx("div",{children:"Products"});export{s as default};
