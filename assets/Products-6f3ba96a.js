3a6406a710423884dad39fc72ff4098fba63d907
MaksymTykhoniuk <MaksymTykhoniuk@users.noreply.github.com>
2023-08-02 17:31:14 +0000

    deploy: 432b568d12872ade10b4d9fa88ebbb11050b6186

---
+++ assets/Products-6f3ba96a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-8ec595a8.js";const s=()=>r.jsx("div",{children:"Products"});export{s as default};
--- assets/Users-e948ee84.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-dbeb02dd.js";const e=()=>s.jsx("div",{children:"Users"});export{e as default};
